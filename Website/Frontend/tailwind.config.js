e753fc9a760239a9dbb03d2db6888f0ec074b24c
Sebastian Wild <sebastian@wild-siena.com>
2024-11-23 16:43:08 +0100

    Setup ASP Core Razor Pages project with TailwindCSS

---
+++ Website/Frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: {
+    relative: true,
+    files: [
+      "../Pages/**/*.cshtml",
+    ]
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
